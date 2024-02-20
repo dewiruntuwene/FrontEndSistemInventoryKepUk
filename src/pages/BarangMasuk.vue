d442803420e384ec44c2b764d8e7af29c3293356
Kolibu Mario Jonatan <90627806+MarioKolibu@users.noreply.github.com>
2024-02-21 00:38:41 +0800

    barang masuk

---
+++ src/pages/BarangMasuk.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Quantity</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(item, index) in items" :key="index">
+            <td>{{ item.id }}</td>
+            <td>{{ item.name }}</td>
+            <td>{{ item.quantity }}</td>
+            <td>{{ item.price }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+    <RouterView />
+  </template>
+  
+  <!-- <script>
+  export default {
+    data() {
+      return {
+        items: [
+          { id: 1, name: 'Item 1', quantity: 10, price: 20.5 },
+          { id: 2, name: 'Item 2', quantity: 15, price: 30.75 },
+          { id: 3, name: 'Item 3', quantity: 5, price: 15.25 }
+        ]
+      };
+    }
+  };
+  </script>
+  
+  <style>
+  /* Add your table styling here */
+  </style> -->
+  
