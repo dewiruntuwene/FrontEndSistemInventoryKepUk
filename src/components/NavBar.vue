701e656a3d2243d38448319ecb5c2d76c7e8c058
dewiruntuwene <71383399+dewiruntuwene@users.noreply.github.com>
2024-05-02 13:18:37 +0700

    Update NavBar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -10,27 +10,15 @@
           <div class="hidden md:block">
             <div class="ml-10 flex items-baseline space-x-4">
               <router-link to="/UserCatalog" class="text-gray-800 hover:text-gray-600">Catalog Barang</router-link>
-              <div class="md:w-32 lg:w-48">
-                <input type="text" class="w-96 rounded-full pl-3 py-1" placeholder="Search">
-              </div>
-              <div class="flex-row ">
-              <router-link to="/logout">
-                <a href="#" class="inline-block text-sm px-4 py-2 leading-none border-black rounded text-black border-white hover:border-transparent hover:text-natural-900 hover:bg-white mt-4 lg:mt-0">Logout</a>
+              
+              <div class="flex-row relative">
+              <router-link to="/login">
+                <a href="#" class=" inline-block text-sm px-4 py-2 leading-none border-black rounded text-black border-white hover:border-transparent hover:text-natural-900 hover:bg-blue mt-4 lg:mt-0">Logout</a>
               </router-link>
           </div>
             </div>
           </div>
           
-          <!-- <div class="ml-auto flex">
-            <div class="flex items-center space-x-4 ">
-              <router-link to="/UserOrder" class="relative text-gray-800 hover:text-gray-600">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="file: mt-4 h-6 w-6">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
-                </svg>
-                <span class="bg-green-500 text-white rounded-full px-2 py-1">{{ updateKeranjang ? updateKeranjang.length : keranjangs.length }}</span>
-              </router-link>
-            </div>
-          </div> -->
 
           <!-- component -->
           <div class="h-screen flex items-center">
