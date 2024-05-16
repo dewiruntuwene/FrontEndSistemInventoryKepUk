7eb47dec213d40c92d10f4fc71ecc87de04044fb
dewiruntuwene <71383399+dewiruntuwene@users.noreply.github.com>
2024-05-16 23:11:25 +0700

    Update register

--- src/pages/register.vue
+++ src/pages/register.vue
@@ -37,7 +37,7 @@ const register = async () => {
 <template>
   <div class="bg-gray-100 min-h-screen flex items-center justify-center">
     <div class="bg-white p-8 rounded-lg shadow-md w-80">
-      <h2 class="text-2xl font-semibold mb-4">Registrasi</h2>
+      <h2 class="text-2xl font-semibold mb-4 text-center">Register</h2>
       <form @submit.prevent="register">
         <div class="mb-4">
           <label for="username" class="block text-sm font-medium text-gray-700">Username</label>
@@ -55,8 +55,9 @@ const register = async () => {
           <label for="confpassword" class="block text-sm font-medium text-gray-700">Konfirmasi Password</label>
           <input type="password" id="confpassword" v-model="confpassword" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50">
         </div>
-        <div class="flex items-center justify-between">
-          <button type="submit" class="bg-blue-500 text-white py-2 px-4 rounded focus:outline-none focus:bg-blue-600 hover:bg-blue-600">Daftar</button>
+        <div  class="mt-4 text-gray-600 text-center">Sudah Punya Account Silahkan <router-link to="/login" class="underline">Login</router-link>.</div>
+        <div class="flex flex-col items-center justify-between">
+          <button type="submit" class="mt-5 items-center bg-blue-500 text-white py-2 px-4 rounded focus:outline-none focus:bg-blue-600 hover:bg-blue-600">Daftar</button>
         </div>
         <!-- Pemberitahuan registrasi berhasil -->
         <div v-if="registrationSuccess" class="mt-4 text-green-600">Registrasi berhasil! Silakan <router-link to="/login" class="underline">login</router-link>.</div>
