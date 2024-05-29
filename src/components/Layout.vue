65959da4c0a1aec70729025219b797f3a78bc6ee
dewiruntuwene <71383399+dewiruntuwene@users.noreply.github.com>
2024-05-29 22:33:22 +0700

    Update Layout.vue

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,42 +1,7 @@
 <script setup lang="ts">
-import axios from 'axios';
-import { ref } from 'vue';
-
-const apiUrl = import.meta.env.VITE_API_URL;
-
   function Open() {
     document.querySelector(".sidebar")?.classList.toggle('left-[-200px]');
   }
-
-const logout = async () => {
-  const token = localStorage.getItem('token');
-  console.log('Token from localStorage:', token); // Debugging tambahan
-  if (!token) {
-    console.error('No token found');
-    return;
-  }
-  try {
-    const response = await axios.delete(`${apiUrl}/logout`, {
-      headers: {
-        'Authorization': `Bearer ${token}`
-      }
-    });
-
-    if (response.status === 200) { // Memeriksa respons status
-      localStorage.removeItem('token');
-      // Redirect to login page or perform any other action
-      window.location.href = '/login';
-    } else {
-      console.error('Logout failed', response.data);
-      // Handle logout failure
-    }
-  } catch (error) {
-    console.error('Logout error:', error);
-    // Handle error
-  }
-}
-
-
 </script>
 
 <template>
@@ -44,7 +9,54 @@ const logout = async () => {
     
     <div class="sm:flex sm:w-full flex-col p-4">
       <!-- Header --> 
-     
+      <div class="lg:left-0 left-[-200px] fixed top-0 z-50 w-full pl-60 flex items-center justify-between bg-slate-200 p-5 ">
+        <div class="flex space-x-5">
+          <button
+            class="lg:hidden flex cursor pointer inline-block rounded bg-blue-600 px-6 py-2.5 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg" onclick="Open()"
+            data-te-sidenav-toggle-ref
+            data-te-target="#sidenav-8"
+            aria-controls="#sidenav-8"
+            aria-haspopup="true">
+            <span class="block [&>svg]:h-5 [&>svg]:w-5 [&>svg]:text-white">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                class="h-5 w-5">
+                <path
+                  fill-rule="evenodd"
+                  d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
+                  clip-rule="evenodd" />
+              </svg>
+            </span>
+          </button>
+          <div>
+            <router-link to="/">
+              Logo
+            </router-link>
+          </div>
+          <div class="md:w-32 lg:w-48">
+            <input type="text" class="w-96 rounded-full pl-3 py-1" placeholder="Search">
+          </div>
+        </div>
+
+        <div class="py-2 flex space-x-3">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            class="h-8 w-8">
+            <path
+              d="M21.721 12.752a9.711 9.711 0 00-.945-5.003 12.754 12.754 0 01-4.339 2.708 18.991 18.991 0 01-.214 4.772 17.165 17.165 0 005.498-2.477zM14.634 15.55a17.324 17.324 0 00.332-4.647c-.952.227-1.945.347-2.966.347-1.021 0-2.014-.12-2.966-.347a17.515 17.515 0 00.332 4.647 17.385 17.385 0 005.268 0zM9.772 17.119a18.963 18.963 0 004.456 0A17.182 17.182 0 0112 21.724a17.18 17.18 0 01-2.228-4.605zM7.777 15.23a18.87 18.87 0 01-.214-4.774 12.753 12.753 0 01-4.34-2.708 9.711 9.711 0 00-.944 5.004 17.165 17.165 0 005.498 2.477zM21.356 14.752a9.765 9.765 0 01-7.478 6.817 18.64 18.64 0 001.988-4.718 18.627 18.627 0 005.49-2.098zM2.644 14.752c1.682.971 3.53 1.688 5.49 2.099a18.64 18.64 0 001.988 4.718 9.765 9.765 0 01-7.478-6.816zM13.878 2.43a9.755 9.755 0 016.116 3.986 11.267 11.267 0 01-3.746 2.504 18.63 18.63 0 00-2.37-6.49zM12 2.276a17.152 17.152 0 012.805 7.121c-.897.23-1.837.353-2.805.353-.968 0-1.908-.122-2.805-.353A17.151 17.151 0 0112 2.276zM10.122 2.43a18.629 18.629 0 00-2.37 6.49 11.266 11.266 0 01-3.746-2.504 9.754 9.754 0 016.116-3.985z" />
+          </svg>
+
+          <div class="flex-row ">
+            <router-link to="/logout">
+              <a href="#" class="inline-block text-sm px-4 py-2 leading-none border-black rounded text-black border-white hover:border-transparent hover:text-natural-900 hover:bg-white mt-4 lg:mt-0">Logout</a>
+            </router-link>
+          </div>
+        </div>       
+      </div>
 
       <div class="flex md:hidden">
         <button id="hamburger">
