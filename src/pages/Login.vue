0988ec586eb762272c43d57ada5eacfe12822498
dewiruntuwene <71383399+dewiruntuwene@users.noreply.github.com>
2024-05-29 22:10:39 +0700

    Update google auth

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -2,176 +2,179 @@
 import axios from 'axios';
 import { onMounted, ref } from 'vue';
 import { useRouter } from 'vue-router';
-import { jwtDecode, JwtPayload } from 'jwt-decode';
-import { GoogleAuth, JWT, OAuth2Client } from 'google-auth-library'
+import { JwtPayload,jwtDecode } from 'jwt-decode';
 
 const router = useRouter();
 const email = ref('');
 const password = ref('');
-
+const errorMessage = ref(''); // Empty error message initially
 
 interface CustomJwtPayload extends JwtPayload {
   role: string;
 }
 
-// Mengambil URL API dari environment variables
 const apiUrl = import.meta.env.VITE_API_URL;
 
-// Fungsi untuk mengarahkan pengguna ke URL otorisasi Google
-const googleAuth = (): void => {
-  if (apiUrl) {
-    redirectToUserCatalog();
-  } else {
-    console.error('API URL is not defined');
+const navigate = (url: string) => {
+  window.location.href = url;
+};
+
+const urlParams = new URLSearchParams(window.location.search);
+console.log('this url:',urlParams)
+const token: string = urlParams.get('token') || '';
+
+// const decodeTokenAndRedirect  = async (token:string) => {
+
+//   try {
+//     // Dekode token (anda memerlukan library JWT untuk ini)
+//     const decodedToken : CustomJwtPayload = jwtDecode(token);
+
+//     // Periksa rolenya
+//     if (decodedToken.role === 'ADMIN') {
+//       // Jika role adalah ADMIN, arahkan ke halaman dashboard
+//       navigate('http://localhost:5173/dashboard');
+//     } else {
+//       // Jika role adalah USER, arahkan ke halaman UserCatalog dengan menyertakan token sebagai query parameter
+//       navigate(`http://localhost:5173/userCatalog?token=${token}`);
+//     }
+//   } catch (error) {
+//     console.error('Error decoding token:', error);
+//   }
+// };
+
+// Fungsi untuk mengarahkan pengguna ke Google untuk otentikasi
+const auth = async () => {
+  try {
+    const response = await fetch('http://localhost:5000/auth/google', { method: 'POST' });
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const data = await response.json();
+    console.log(data)
+
+    // Logging untuk debugging
+    console.log('Data dari API:', data);
+
+    if (data.url) {
+      // Redirect ke URL otentikasi Google
+      window.location.href = data.url;
+    } else {
+      throw new Error('Response data does not contain URL');
+    }
+  } catch (error) {
+    console.error('There was a problem with the fetch operation:', error);
   }
 };
 
-// Fungsi untuk melakukan redirect ke halaman autentikasi Google
-const redirectToUserCatalog = async (): Promise<void> => {
-  const response = await axios.get(`${apiUrl}/auth/google/`, { withCredentials: true });
-  const authUrl = response.data.url;
-  window.location.href = authUrl;
-    
-};
-
 // Fungsi untuk memeriksa token setelah redirect
-const checkToken = async (): Promise<void> => {
+const checkToken = () => {
   const urlParams = new URLSearchParams(window.location.search);
+  console.log('URL Params:', urlParams);
+
   const token = urlParams.get('token');
+  console.log('Token:',token);
 
   if (token) {
-      try {
-        // Decode token
-        const decodedToken:CustomJwtPayload = jwtDecode(token);
-        console.log('Decoded Token:', decodedToken);
+    try {
+      // Decode token menggunakan tipe khusus
+      const decodedToken : CustomJwtPayload = jwtDecode(token);
+      console.log('Decoded Token:', decodedToken);
 
-        // Simpan token ke local storage
-        localStorage.setItem('token', token);
+      // Simpan token ke local storage
+      localStorage.setItem('token', token);
 
-        // Check role and redirect accordingly
-        const userRole = decodedToken.role;
-        if (userRole === 'USER') {
-          router.push('/UserCatalog');
-        } else if (userRole === 'ADMIN') {
-          router.push('/Dashboard');
-        } else {
-          console.error('Unknown role:', userRole);
-        }
-      } catch (error) {
-        console.error('Failed to decode token:', error);
+      // Check role and redirect accordingly
+      const userRole = decodedToken.role;
+      if (userRole === 'USER') {
+        router.push('/UserCatalog');
+      } else if (userRole === 'ADMIN') {
+        router.push('/Dashboard');
+      } else {
+        console.error('Unknown role:', userRole);
       }
-    } else {
-      console.error('No token found in URL');
+    } catch (error) {
+      console.error('Failed to decode token:', error);
     }
+  } else {
+    console.error('No token found in URL');
+  }
 };
 
-interface User {
-  name: string;
-  email: string;
-}
+window.onload = checkToken;
 
-// const user = ref<User | null>(null);
+// // Panggil fungsi auth untuk memulai proses
+// auth();
 
-//     const getUser = async () => {
-//       try {
-//         const url = `${apiUrl}/auth/login/`;
-//         const { data } = await axios.get(url, { withCredentials: true });
-//         user.value = data.user._json;
-//       } catch (err) {
-//         console.log(err);
-//       }
-//     };
-
-
-//Jika menggunakan Vue.js, panggil checkToken setelah komponen dimuat
-onMounted(() => {
-  checkToken();
-});
 
 
 
 const login = async () => {
   try {
-    console.log('Email:', email.value);
-    console.log('Password:', password.value);
     const response = await axios.post(`${apiUrl}/login`, { email: email.value, password: password.value });
-    console.log('Response:', response.data);
-
-    // Dekode token JWT untuk mendapatkan informasi peran pengguna
     const decodedToken: CustomJwtPayload = jwtDecode(response.data.token);
-    console.log('Decoded Token:', decodedToken); // Debugging statement
+
     if (!decodedToken || !decodedToken.role) {
       console.error('Token does not contain role information.');
-      return; // Exit the function
+      return;
     }
 
     const userRole = decodedToken.role;
-    console.log('User Role:', userRole); // Debugging statement
-
-    // Redirect user based on role after successful login
     if (userRole === 'USER') {
       router.push('/UserCatalog');
     } else if (userRole === 'ADMIN') {
       router.push('/Dashboard');
     }
-    // Arahkan pengguna sesuai peran setelah login berhasil
-    if (userRole  === 'USER') {
-      router.push('/UserCatalog');
-    } else if (userRole  === 'ADMIN') {
-      router.push('/Dashboard');
-    }
 
-    // Simpan token ke local storage untuk digunakan pada permintaan berikutnya
     localStorage.setItem('token', response.data.token);
-    // Cek apakah token sudah tersimpan di local storage
-    const token = localStorage.getItem('token');
-    if (token) {
-      // Token sudah tersimpan di local storage
-      console.log('Token sudah tersimpan:', token);
-      // Lakukan penanganan sesuai kebutuhan, misalnya, lanjutkan ke halaman berikutnya
-    } else {
-      // Token belum tersimpan di local storage
-      console.log('Token belum tersimpan.');
-    }
-    
-
-    
-    // Set header otorisasi untuk permintaan selanjutnya
     axios.defaults.headers.common['Authorization'] = `Bearer ${response.data.token}`;
-
-    // Verifikasi bahwa token telah diset di header
-    console.log('Header Authorization telah diset:', axios.defaults.headers.common['Authorization']);
-
-
     alert('Login berhasil!');
   } catch (error) {
     console.error('Error:', error);
-    // Lakukan penanganan kesalahan sesuai kebutuhan
+    if (axios.isAxiosError(error) && error.response) {
+      const status = error.response.status;
+      if (status === 401) {
+        errorMessage.value = 'Email atau password salah.';
+      } else if (status === 404) {
+        errorMessage.value = 'Username tidak ditemukan.';
+      } else {
+        errorMessage.value = 'Terjadi kesalahan pada server.';
+      }
+    } else {
+      errorMessage.value = 'Terjadi kesalahan pada server.';
+    }
   }
 };
+
+
+
+
 </script>
+
 
 <template>
   <div class="bg-gray-100 flex items-center justify-center h-screen">
     <div class="bg-white p-8 rounded-lg shadow-md w-80">
       <h2 class="text-2xl font-semibold mb-4">Klinik Inventory System</h2>
-      <h3 class="text-lg text-gray-600 mb-6 text-center ">Login</h3>
+      <h3 class="text-lg text-gray-600 mb-6 text-center">Login</h3>
       <form @submit.prevent="login">
         <div class="mb-4">
-          <label for="username" class="block text-sm font-medium text-gray-700">Email</label>
+          <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
           <input type="text" id="email" v-model="email" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50">
         </div>
         <div class="mb-4">
           <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
           <input type="password" id="password" v-model="password" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50">
         </div>
-        <div  class="mt-4 text-gray-600 text-center">Belum Punya Account Silahkan <router-link to="/register" class="underline">Register</router-link>.</div>
+        <div v-if="errorMessage" class="text-red-500 text-sm mb-4">{{ errorMessage }}</div>
+        <div class="mt-4 text-gray-600 text-center">Belum Punya Account Silahkan <router-link to="/register" class="underline">Register</router-link>.</div>
         <div class="flex flex-col items-center justify-between">
           <button type="submit" class="mt-5 bg-blue-500 text-white py-2 px-4 rounded focus:outline-none focus:bg-blue-600 hover:bg-blue-600">Login</button>
         </div>
       </form>
       <div>
-        <button @click="googleAuth">Login with Google</button>
+        <button @click="auth" class="mt-4 bg-red-500 text-white py-2 px-4 rounded focus:outline-none focus:bg-red-600 hover:bg-red-600">Login with Google</button>
       </div>
     </div>
   </div>
