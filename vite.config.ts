e90798f1b12a1199bf7ba64c6f015363c5ffc534
dewiruntuwene <71383399+dewiruntuwene@users.noreply.github.com>
2024-07-11 07:26:37 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { createProxyMiddleware } from 'http-proxy-middleware';
-
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { createProxyMiddleware } from "http-proxy-middleware";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/lmsfrontend',
+  base: "/lmsfrontend",
   plugins: [vue()],
   resolve: {
     alias: {
-      '@components': '/src/components',
-    }
-  }
- 
-})
-
-
+      "@components": "/src/components",
+    },
+  },
+});
