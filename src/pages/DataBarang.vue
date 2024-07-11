af147cac1e6fe0727b3f2f8c93785fc2ff2f669d
dewiruntuwene <71383399+dewiruntuwene@users.noreply.github.com>
2024-07-11 07:24:42 +0700

    Update DataBarang.vue

--- src/pages/DataBarang.vue
+++ src/pages/DataBarang.vue
@@ -1,16 +1,22 @@
 <template>
-  <Layout/>
+  <Layout />
   <div class="mt-0 pl-[15rem] pr-4">
-    <div class="border-y-0 border-b-0 bg-white-200 bg-slate-300 flex flex-row justify-between items-center p-5 pl-5">
+    <div
+      class="border-y-0 border-b-0 bg-white-200 bg-slate-300 flex flex-row justify-between items-center p-5 pl-5"
+    >
       <h1 class="text-4xl font-bold pa-3">Data Barang</h1>
     </div>
 
     <div class="flex flex-row mt-4">
       <!-- Tabel dengan data dari API -->
-      <div class="max-w-4xl mr-8 bg-white border-y-2 border-b-4 p-5 overflow-x-auto">
+      <div
+        class="max-w-4xl mr-8 bg-white border-y-2 border-b-4 p-5 overflow-x-auto"
+      >
         <table class="min-w-max w-full table-auto">
           <thead>
-            <tr class="bg-blue-200 text-gray-600 uppercase text-sm leading-normal">
+            <tr
+              class="bg-blue-200 text-gray-600 uppercase text-sm leading-normal"
+            >
               <th class="py-2 px-4 text-left border">Kode Barang</th>
               <th class="py-2 px-4 text-left border">Nama Barang</th>
               <th class="py-2 px-4 text-left border">Total Stok</th>
@@ -20,22 +26,51 @@
             </tr>
           </thead>
           <tbody class="text-gray-600 text-sm font-light">
-            <tr v-for="(item, index) in barangs" :key="index" class="border-b hover:bg-gray-100">
+            <tr
+              v-for="(item, index) in barangs"
+              :key="index"
+              class="border-b hover:bg-gray-100"
+            >
               <td class="py-2 px-4 text-left border">{{ item.kode_barang }}</td>
               <td class="py-2 px-4 text-left border">
-                <input type="text" :value="item.nama_barang" class="border border-gray-300 rounded px-2 py-1 w-full" disabled>
+                <input
+                  type="text"
+                  :value="item.nama_barang"
+                  class="border border-gray-300 rounded px-2 py-1 w-full"
+                  disabled
+                />
               </td>
               <td class="py-2 px-4 text-left border">
-                <input type="number" :value="item.total_stock" class="border border-gray-300 rounded px-2 py-1 w-full" disabled>
+                <input
+                  type="number"
+                  :value="item.total_stock"
+                  class="border border-gray-300 rounded px-2 py-1 w-full"
+                  disabled
+                />
               </td>
               <td class="py-2 px-4 text-left border">
-                <input type="text" :value="item.jenis_barang" class="border border-gray-300 rounded px-2 py-1 w-full" disabled>
+                <input
+                  type="text"
+                  :value="item.jenis_barang"
+                  class="border border-gray-300 rounded px-2 py-1 w-full"
+                  disabled
+                />
               </td>
               <td class="py-2 px-4 text-left border">
-                <input type="number" :value="item.harga_barang" class="border border-gray-300 rounded px-2 py-1 w-full" disabled>
+                <input
+                  type="number"
+                  :value="item.harga_barang"
+                  class="border border-gray-300 rounded px-2 py-1 w-full"
+                  disabled
+                />
               </td>
               <td class="py-2 px-4 text-left border">
-                <img v-if="item.gambar_barang" :src="`http://localhost:5000/uploads/${item.gambar_barang}`" alt="Gambar Barang" class="h-12 w-12 object-cover">
+                <img
+                  v-if="item.gambar_barang"
+                  :src="`http://localhost:5000/uploads/${item.gambar_barang}`"
+                  alt="Gambar Barang"
+                  class="h-12 w-12 object-cover"
+                />
               </td>
             </tr>
           </tbody>
@@ -46,14 +81,51 @@
       <div class="max-w-xs bg-white border-y-2 border-b-4 p-5">
         <h2 class="text-2xl font-bold mb-4">Tambah Barang</h2>
         <div class="flex flex-col">
-          <input type="text" v-model="newItem.kode" class="border border-gray-300 rounded px-2 py-1 mb-2" placeholder="Kode Barang">
-          <input type="text" v-model="newItem.nama" class="border border-gray-300 rounded px-2 py-1 mb-2" placeholder="Nama Barang">
-          <input type="text" v-model="newItem.totalStok" class="border border-gray-300 rounded px-2 py-1 mb-2" placeholder="Total Stok">
-          <input type="number" v-model="newItem.harga" class="border border-gray-300 rounded px-2 py-1 mb-2" placeholder="Harga">
-          <input type="text" v-model="newItem.jenis" class="border border-gray-300 rounded px-2 py-1 mb-2" placeholder="Jenis Barang">
-          <input type="file" @change="handleImageUpload" class="border border-gray-300 rounded px-2 py-1 mb-2" accept="image/*">
-          <button @click="validateAndAddItem" class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 rounded">Add</button>
-          <p v-if="errorMessage" class="text-red-500 mt-2">{{ errorMessage }}</p>
+          <input
+            type="text"
+            v-model="newItem.kode"
+            class="border border-gray-300 rounded px-2 py-1 mb-2"
+            placeholder="Kode Barang"
+          />
+          <input
+            type="text"
+            v-model="newItem.nama"
+            class="border border-gray-300 rounded px-2 py-1 mb-2"
+            placeholder="Nama Barang"
+          />
+          <input
+            type="text"
+            v-model="newItem.totalStok"
+            class="border border-gray-300 rounded px-2 py-1 mb-2"
+            placeholder="Total Stok"
+          />
+          <input
+            type="number"
+            v-model="newItem.harga"
+            class="border border-gray-300 rounded px-2 py-1 mb-2"
+            placeholder="Harga"
+          />
+          <input
+            type="text"
+            v-model="newItem.jenis"
+            class="border border-gray-300 rounded px-2 py-1 mb-2"
+            placeholder="Jenis Barang"
+          />
+          <input
+            type="file"
+            @change="handleImageUpload"
+            class="border border-gray-300 rounded px-2 py-1 mb-2"
+            accept="image/*"
+          />
+          <button
+            @click="validateAndAddItem"
+            class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 rounded"
+          >
+            Add
+          </button>
+          <p v-if="errorMessage" class="text-red-500 mt-2">
+            {{ errorMessage }}
+          </p>
         </div>
       </div>
     </div>
@@ -63,9 +135,11 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
-import axios from 'axios';
+import { defineComponent, ref } from "vue";
+import axios from "axios";
 import Layout from "../components/layout.vue";
+
+const apiUrl = import.meta.env.VITE_API_URL;
 
 interface Barang {
   kode_barang: number;
@@ -78,60 +152,79 @@ interface Barang {
 
 export default defineComponent({
   components: {
-    Layout
+    Layout,
   },
   setup() {
     const barangs = ref<Barang[]>([]);
     const newItem = ref({
-      kode: '',
-      nama: '',
-      totalStok: '',
-      harga: '',
-      jenis: '',
-      image: null
+      kode: "",
+      nama: "",
+      totalStok: "",
+      harga: "",
+      jenis: "",
+      image: null,
     });
-    const errorMessage = ref('');
+    const errorMessage = ref("");
 
     const loadDataFromAPI = async () => {
       try {
-        const response = await axios.get('https://vjk2k0f5-5000.asse.devtunnels.ms/barang');
+        const response = await axios.get(
+          `${apiUrl}/barang`,
+        );
         barangs.value = response.data;
       } catch (error) {
-        console.error('Error loading data:', error);
+        console.error("Error loading data:", error);
       }
     };
 
     const validateAndAddItem = async () => {
-      if (!newItem.value.kode || !newItem.value.nama || !newItem.value.totalStok || !newItem.value.harga || !newItem.value.jenis || !newItem.value.image) {
-        errorMessage.value = 'Harap isi semua kolom sebelum menambahkan!';
+      if (
+        !newItem.value.kode ||
+        !newItem.value.nama ||
+        !newItem.value.totalStok ||
+        !newItem.value.harga ||
+        !newItem.value.jenis ||
+        !newItem.value.image
+      ) {
+        errorMessage.value = "Harap isi semua kolom sebelum menambahkan!";
       } else if (checkDuplicateKode(newItem.value.kode)) {
-        errorMessage.value = 'Kode barang sudah ada!';
+        errorMessage.value = "Kode barang sudah ada!";
       } else {
         await addItem();
-        errorMessage.value = '';
+        errorMessage.value = "";
       }
     };
 
     const addItem = async () => {
       const formData = new FormData();
-      formData.append('kode_barang', newItem.value.kode);
-      formData.append('nama_barang', newItem.value.nama);
-      formData.append('total_stock', newItem.value.totalStok);
-      formData.append('harga_barang', newItem.value.harga);
-      formData.append('jenis_barang', newItem.value.jenis);
-      formData.append('gambar_barang', newItem.value.image);
+      formData.append("kode_barang", newItem.value.kode);
+      formData.append("nama_barang", newItem.value.nama);
+      formData.append("total_stock", newItem.value.totalStok);
+      formData.append("harga_barang", newItem.value.harga);
+      formData.append("jenis_barang", newItem.value.jenis);
+      formData.append("gambar_barang", newItem.value.image);
 
       try {
-        await axios.post('https://vjk2k0f5-5000.asse.devtunnels.ms/barang', formData);
-        newItem.value = { kode: '', nama: '', totalStok: '', harga: '', jenis: '', image: null };
+        await axios.post(
+          `${apiUrl}/barang`,
+          formData,
+        );
+        newItem.value = {
+          kode: "",
+          nama: "",
+          totalStok: "",
+          harga: "",
+          jenis: "",
+          image: null,
+        };
         await loadDataFromAPI();
       } catch (error) {
-        console.error('Error adding item:', error);
+        console.error("Error adding item:", error);
       }
     };
 
     const checkDuplicateKode = (kode: string) => {
-      return barangs.value.some(item => item.kode_barang.toString() === kode);
+      return barangs.value.some((item) => item.kode_barang.toString() === kode);
     };
 
     const handleImageUpload = (event: any) => {
@@ -145,9 +238,9 @@ export default defineComponent({
       newItem,
       errorMessage,
       validateAndAddItem,
-      handleImageUpload
+      handleImageUpload,
     };
-  }
+  },
 });
 </script>
 
@@ -157,7 +250,8 @@ table {
   width: 100%;
 }
 
-th, td {
+th,
+td {
   border: 1px solid #ddd;
   text-align: left;
 }
