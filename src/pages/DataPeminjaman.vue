c57d1786c18d125c18dfcc964972156a37d442bb
dewiruntuwene <71383399+dewiruntuwene@users.noreply.github.com>
2024-07-17 12:41:21 +0700

    Update DataPeminjaman.vue

--- src/pages/DataPeminjaman.vue
+++ src/pages/DataPeminjaman.vue
@@ -121,7 +121,7 @@
                 <td class="px-2 py-2 whitespace-nowrap text-center">
                   <template v-if="transaction.status === 'pending'">
                     <button
-                      v-if="transaction.barangHabisPakai[0].jenis_barang === 'Habis Pakai'"
+                      v-if="transaction.barangHabisPakai[0].jenis_barang === 'Barang Habis Pakai'"
                       @click="updateTransactionOrder(Number(transaction.id_peminjam))"
                       type="button"
                       class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
